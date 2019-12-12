2341ba238908f4b7b74095cee5f32e348a71fcd1
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-12 08:29:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,16 +3,18 @@
 
 def fizzbuzz(int)
   
-  if (int % 3 == 0 && int % 5 != 0)
+  if (int % 3 == 0 && int % 5 == 0)
+    return "FizzBuzz"
+  end
+  
+  if (int % 3 == 0)
     "Fizz"
   end
   
-  if (int % 5 == 0 && int % 3 != 0)
+  if (int % 5 == 0)
     "Buzz"
   end
   
-  if (int % 3 == 0 && int % 5 == 0)
-    "FizzBuzz"
-  end
+  
   
 end
